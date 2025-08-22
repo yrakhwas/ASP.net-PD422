78a7cbb7bb769a0337244be161c9b7d4b96bbdb8
yrakhwas <yrakhwas@gmail.com>
2025-08-22 15:31:23 +0300

    Add DataAccess

---
+++ DataAccess/ShopPD422Db.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess
+{
+    public class ShopPD422Db: DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
